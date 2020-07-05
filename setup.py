90cc0455dccd051fa50ee1df082e8aa5cbb8327a
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-07-05 13:33:36 -0300

    :art: Format setup.py code.

--- setup.py
+++ setup.py
@@ -1,42 +1,49 @@
 """
 Setup
 """
-import os
-import setuptools
 import distutils.cmd
 import distutils.log
+import os
 import subprocess
 
+import setuptools
+
+
 class UpgradeCommand(distutils.cmd.Command):
-  """A custom command to run pip-compile generating hashes and outputting to requirements-lock.txt."""
+    """A custom command to run pip-compile generating hashes and outputting to
+    requirements-lock.txt."""
 
-  description = 'update requirements-lock.txt with upgraded packages'
-  user_options = [
-      # The format is (long option, short option, description).
-      ('output-file=', None, 'path to output requirements file'),
-  ]
+    description = 'update requirements-lock.txt with upgraded packages'
+    user_options = [
+        # The format is (long option, short option, description).
+        ('output-file=', None, 'path to output requirements file'),
+    ]
 
-  def initialize_options(self):
-    """Set default values for options."""
-    # Each user option must be listed here with their default value.
-    self.output_file = 'requirements-lock.txt'
+    def initialize_options(self):
+        """Set default values for options."""
+        # Each user option must be listed here with their default value.
+        self.output_file = 'requirements-lock.txt'
 
-  def finalize_options(self):
-    """Post-process options."""
-    if self.output_file:
-      assert os.path.exists(self.output_file), (
-          'Output file %s does not exist.' % self.output_file)
+    def finalize_options(self):
+        """Post-process options."""
+        if self.output_file:
+            assert os.path.exists(self.output_file), (
+                'Output file %s does not exist.' % self.output_file)
 
-  def run(self):
-    """Run command."""
-    command = ['pip-compile']
-    if self.output_file:
-      command.append('--find-links=https://download.pytorch.org/whl/torch_stable.html --generate-hashes --upgrade --output-file=%s' % self.output_file)
-    command.append(os.getcwd())
-    self.announce(
-        'Running command: %s' % str(command),
-        level=distutils.log.INFO)
-    subprocess.check_call(command)
+    def run(self):
+        """Run command."""
+        command = ['pip-compile']
+        if self.output_file:
+            # pylint: disable=line-too-long
+            command.append(
+                '--find-links=https://download.pytorch.org/whl/torch_stable.html --generate-hashes --upgrade --output-file=%s' % self.output_file)
+        command.append(os.getcwd())
+        print(command)
+        self.announce(
+            'Running command: %s' % str(command),
+            level=distutils.log.INFO)
+        subprocess.check_call(command)
+
 
 def package_files(directory):
     """package_files
@@ -49,6 +56,7 @@ def package_files(directory):
         for filename in filenames:
             paths.append(os.path.join('..', path, filename))
     return paths
+
 
 extra_files = package_files('time_series_predictor/sklearn')
 
@@ -78,7 +86,7 @@ setuptools.setup(
     install_requires=[
         'torch===1.5.0',
         'skorch',
-        'scipy==1.4.1', # via skorch
+        'scipy==1.4.1',  # via skorch
         'psutil'
     ],
     extras_require={
