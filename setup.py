8816bd083478f3055b35377d559bb93841763239
Your Name <daniel@kryptonunite.com>
2020-10-10 21:31:37 +0100

    :construction: Development path.

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://timeseriespredictor.readthedocs.io/",
-    packages=['time_series_predictor', 'src', 'src/oze_dataset', 'src/oze_dataset/labels.json'],
+    packages=['time_series_predictor', 'src', 'src/oze_dataset', 'src/oze_dataset/labels'],
     package_data={'': extra_files},
     classifiers=[
         "Programming Language :: Python :: 3",
