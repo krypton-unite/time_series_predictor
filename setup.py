f397a1ff84b94851707cc89a1c1bafd3d76a86db
Your Name <daniel@kryptonunite.com>
2020-10-10 21:32:51 +0100

    :construction: Development path.

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ def package_files(directory):
     return paths
 
 
-extra_files = package_files('time_series_predictor/sklearn')
+extra_files = package_files('src/oze_dataset/labels.json', 'time_series_predictor/sklearn')
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -105,7 +105,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://timeseriespredictor.readthedocs.io/",
-    packages=['time_series_predictor', 'src', 'src/oze_dataset', 'src/oze_dataset/labels'],
+    packages=['time_series_predictor', 'src', 'src/oze_dataset'],
     package_data={'': extra_files},
     classifiers=[
         "Programming Language :: Python :: 3",
