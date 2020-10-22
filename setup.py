4a24414d341e6e001dc8a901298d8c714e256173
Your Name <daniel@kryptonunite.com>
2020-10-22 22:20:46 +0100

    Bump version: 1.4.1 → 1.4.2

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ setup(
         'synchronize': SyncCommand
     },
     name="time_series_predictor",
-    version="1.4.1",
+    version="1.4.2",
     author="Daniel Kaminski de Souza",
     author_email="daniel@kryptonunite.com",
     description="Time Series Predictor",
