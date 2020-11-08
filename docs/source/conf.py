904ed51e8252b1196ac046c24d39618d5cdcbbc6
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-11-08 21:10:42 +0000

    Bump version: 2.1.0 → 2.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Daniel Kaminski de Souza'
 author = 'Daniel Kaminski de Souza'
 
 # The full version, including alpha/beta/rc tags
-version = '2.1.0'
+version = '2.1.1'
 release = version
 
 
