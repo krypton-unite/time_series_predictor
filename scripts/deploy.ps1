44c320df386e026f231871a6d55d2376fb4b71a3
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-17 02:03:51 +0000

    Bump version: 3.0.2 → 3.0.3

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,5 +1,5 @@
 python setup.py sdist bdist_wheel
-$version="3.0.2"
+$version="3.0.3"
 $files_to_handle_str="dist/time_series_predictor-$version*" 
 twine check $files_to_handle_str
 twine upload $files_to_handle_str
