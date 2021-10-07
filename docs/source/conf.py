abcaf20ed2bd37fc65b74094a9373a7a1bac19ee
Daniel Kaminski de Souza <daniel.k.de.souza@gmail.com>
2021-10-07 23:07:21 +0100

    Bump version: 3.0.5 → 3.0.6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Daniel Kaminski de Souza'
 author = 'Daniel Kaminski de Souza'
 
 # The full version, including alpha/beta/rc tags
-version = '3.0.5'
+version = '3.0.6'
 release = version
 
 
--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,5 +1,5 @@
 python setup.py sdist bdist_wheel
-$version="3.0.5"
+$version="3.0.6"
 $files_to_handle_str="dist/time_series_predictor-$version*" 
 twine check $files_to_handle_str
 twine upload $files_to_handle_str
